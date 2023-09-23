4f37cb95ceb4c938d4cecade0d31d8741354c939
Alejandro Roa <alejodev04@gmail.com>
2023-09-23 10:12:38 -0400

    Refactor/team component (#1)
    * refactor, wip
    
    * fix refactor validation
    
    * disabled endpoints prod
    
    ---------
    
    Co-authored-by: Alejandro Roa <alejandro.roa@workiasolutions.com>

--- src/app/team/components/team.component.ts
+++ src/app/team/components/team.component.ts
@@ -3,16 +3,15 @@ import { TeamService } from "../services/team.service";
 import { League } from "../interfaces/league.interface";
 import { Player } from "../interfaces/player.interface";
 import { Subject, Subscription, debounceTime, distinctUntilChanged, switchMap } from "rxjs";
-import { TeamLimits, Position } from '../enums/enums';
-import { playerActions } from '../interfaces/playerAction.interface'
 import { SpinnerService } from '../services/spinnerService';
+import { UtilSavePlayer } from "../utils/savePlayer.util";
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit, OnDestroy {
+export class TeamComponent extends UtilSavePlayer implements OnInit, OnDestroy {
   leagues: League[] = [];
   player!: Player;
   selectedLeagueID: number = 0;
@@ -20,16 +19,9 @@ export class TeamComponent implements OnInit, OnDestroy {
   showLeagues: boolean = true;
   showSelectedLeague: boolean = false;
   showPlayer: boolean = false;
-  savedPlayer: boolean = false;
-  playerValidate: boolean = false;
   playerDataSubject = new Subject;
-  msgValidation: string = "";
-  msgPositionValidation: string = "";
-  msgCountValidation: string = "";
-  msgHasPlayer: string = "";
   leagueInput: string = "";
   msgCompleteTeam: string = "";
-  quantityPlayers: number = 0;
   playerNotFound: boolean = false;
   leagueNotFound: boolean = false;
   leagueSubscription: Subscription = new Subscription;
@@ -39,9 +31,12 @@ export class TeamComponent implements OnInit, OnDestroy {
 
   constructor(
     public spinnerService: SpinnerService ,
-    private teamService: TeamService
-  ) { }
-
+    teamService: TeamService
+  ) {
+    super(teamService);
+    this.teamService = teamService;
+  }
+ 
   ngOnInit() {
     this.searchLeaguesDebounce();
     this.searchPlayersDebounce();
@@ -101,180 +96,7 @@ export class TeamComponent implements OnInit, OnDestroy {
     this.showLeagues = false;
     this.showSelectedLeague = true;
   }
-
-  savePlayer() {
-    const localStorageData = localStorage.getItem('team');
-    let existingData = [];
-    let hasPlayer:boolean = false;
-    let newData = this.player;
-
-    if(localStorageData){
-      existingData = JSON.parse(localStorageData);
-      hasPlayer = existingData.find((player: Player) => player.id === this.player.id);
-      this.quantityPlayers = existingData.length;
-    }
-
-    if (existingData.length > 0 && !hasPlayer) {
-      const getMaxPlayerForTeam = this.getMaxPlayerForTeam(existingData, newData.teamID, newData.position);
-
-      if (existingData.length > 16) {
-        this.msgValidation = "máximo 16 players por equipo"
-        return
-      }
-
-      if (!getMaxPlayerForTeam.saveMoreIDPlayers ||
-        !getMaxPlayerForTeam.saveMoreAttacker ||
-        !getMaxPlayerForTeam.saveMoreDefenders ||
-        !getMaxPlayerForTeam.saveMoreGoalkeeper ||
-        !getMaxPlayerForTeam.saveMoreMidfielder) {
-        this.msgValidation = "No puede guardar este jugador"
-        this.playerValidate = true;
-        return
-      }
-
-      if (Array.isArray(existingData)) {
-        this.teamService.addPlayer(newData);
-        existingData.push(newData);
-        localStorage.setItem('team', JSON.stringify(existingData));
-        this.savedPlayer = true;
-        return
-      }
-
-    } else {
-      if (hasPlayer) {
-        this.playerValidate = true;
-        this.msgHasPlayer = "No podemos fichar dos veces al mismo jugador";
-        return
-      }
-
-      this.teamService.addPlayer(newData);
-      localStorage.setItem('team', JSON.stringify([newData]));
-      this.savedPlayer = true;
-    }
-  }
-
-
-  getMaxPlayerForTeam(existingData: Player[], teamID: number, position: string) {
-    let teamsIDs: number[] = this.getTeamsID(existingData)
-    let positions: string[] = this.getTeamPositions(existingData);
-    let fullTeamsIDs: any[] = [];
-    let fullTeamPositions: any[] = [];
-
-
-    const countTeamIDs = teamsIDs.reduce((count: any, value) => {
-      count[value] = (count[value] || 0) + 1;
-      return count;
-    }, {});
-
-    const teamPositions = positions.reduce((count: any, value) => {
-      count[value] = (count[value] || 0) + 1;
-      return count;
-    }, {});
-
-    for (const value in countTeamIDs) {
-      if (countTeamIDs.hasOwnProperty(value)) {
-        if (countTeamIDs[value] >= TeamLimits.maxPlayerForTeam) {
-          if (parseInt(value) === teamID) {
-            this.msgCountValidation = "No puede guardar más jugadores de este equipo"
-            fullTeamsIDs.push(value)
-            playerActions.saveMoreIDPlayers = false;
-          } else {
-            playerActions.saveMoreIDPlayers = true;
-          }
-        }
-      }
-    }
-
-    if (position === Position.Defender) {
-      for (const value in teamPositions) {
-        if (teamPositions.hasOwnProperty(value)) {
-          if (teamPositions[Position.Defender] >= TeamLimits.maxDefendersForTeam &&
-            value === position) {
-            this.msgPositionValidation = `No puede guardar más ${value}`
-            fullTeamPositions.push(value)
-            playerActions.saveMoreDefenders = false;
-          } else {
-            playerActions.saveMoreDefenders = true;
-          }
-        }
-      }
-    }
-
-    if (position === Position.Midfielder) {
-      for (const value in teamPositions) {
-        if (teamPositions.hasOwnProperty(value)) {
-          if (teamPositions[Position.Midfielder] >= TeamLimits.maxMidfieldersForTeam &&
-            value === position) {
-            this.msgPositionValidation = `No puede guardar más ${value}`
-            fullTeamPositions.push(value)
-            playerActions.saveMoreMidfielder = false;
-          } else {
-            playerActions.saveMoreMidfielder = true;
-          }
-        }
-      }
-    }
-
-    if (position === Position.Goalkeeper) {
-      for (const value in teamPositions) {
-        if (teamPositions.hasOwnProperty(value)) {
-          if (teamPositions[Position.Goalkeeper] >= TeamLimits.maxGoalkeepersForTeam &&
-            value === position) {
-            this.msgPositionValidation = `No puede guardar más ${value}`
-            fullTeamPositions.push(value)
-            playerActions.saveMoreGoalkeeper = false;
-          } else {
-            playerActions.saveMoreGoalkeeper = true;
-          }
-        }
-      }
-    }
-
-    if (position === Position.Attacker) {
-      for (const value in teamPositions) {
-        if (teamPositions.hasOwnProperty(value)) {
-          if (teamPositions[Position.Attacker] >= TeamLimits.maxAttackersForTeam) {
-            this.msgPositionValidation = `No puede guardar más ${value}`
-            fullTeamPositions.push(value)
-            playerActions.saveMoreAttacker = false;
-          } else {
-            playerActions.saveMoreAttacker = true;
-          }
-        }
-      }
-    }
-
-    const saveMoreAttacker = playerActions.saveMoreAttacker;
-    const saveMoreDefenders = playerActions.saveMoreDefenders;
-    const saveMoreMidfielder = playerActions.saveMoreMidfielder;
-    const saveMoreGoalkeeper = playerActions.saveMoreGoalkeeper;
-    const saveMoreIDPlayers = playerActions.saveMoreIDPlayers;
-
-    return {
-      saveMoreIDPlayers,
-      fullTeamsIDs,
-      saveMoreDefenders,
-      saveMoreMidfielder,
-      saveMoreGoalkeeper,
-      saveMoreAttacker,
-      fullTeamPositions
-    }
-  }
-
-  getTeamsID(existingData: Player[]) {
-    let teamsIDs: number[] = [];
-    teamsIDs = existingData.map((item: Player) => item.teamID);
-
-    return teamsIDs;
-  }
-
-  getTeamPositions(existingData: Player[]) {
-    let positions: string[] = [];
-    positions = existingData.map((item: Player) => item.position);
-
-    return positions;
-  }
-
+  
   cleanAllState() {
     this.showPlayer = false;
     this.savedPlayer = false;
