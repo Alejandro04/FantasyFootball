4692a8f2543719d634ae6c822e26b2cbe371a376
Alejandro Roa <alejandro.roa@workiasolutions.com>
2023-09-27 17:39:46 -0400

    add and delete players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,32 +12,62 @@ export class AppComponent implements OnInit {
   countries: Team[] = [];
   players: Player[] = [];
   coach: any;
+  goalKeepers: any[] = []
 
   constructor(
     private teamService: TeamV2Service
-  ){}
+  ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getCountries();
     this.getTeam();
     this.getCoach();
   }
 
-  getCountries(){
+  getCountries() {
     this.teamService.getCountries().subscribe((countries) => {
       this.countries = countries;
     })
   }
 
-  getTeam(){
+  getTeam() {
     this.teamService.getTeam(1).subscribe((players) => {
       this.players = players;
     })
   }
 
-  getCoach(){
+  getCoach() {
     this.teamService.getCoach(1).subscribe((coach) => {
       this.coach = coach[0];
     })
+  }
+
+  savePlayer(player: any) {
+    if (player.position === 'Goalkeeper' && this.goalKeepers.length <= 3) {
+
+      const savedPlayer = this.goalKeepers.find((item) => {
+        return item.id === player.id
+      })
+
+      if (savedPlayer) return
+
+      this.goalKeepers = [
+        ...this.goalKeepers,
+        {
+          id: player.id,
+          name: player.name,
+          photo: player.photo,
+          position: player.position
+        }
+      ]
+    }
+  }
+
+  deletePlayer(player:any){
+    if(player.position === 'Goalkeeper'){
+      this.goalKeepers = this.goalKeepers.filter((item) => {
+        return item.id !== player.id;
+      })
+    }
   }
 }
